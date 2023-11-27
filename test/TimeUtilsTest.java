42b34f781d55a091d73002802cb3eebdc55997d1
Eetu Soronen <89025008+soronen@users.noreply.github.com>
2023-11-27 21:11:53 +0200

    update

--- test/TimeUtilsTest.java
+++ test/TimeUtilsTest.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TimeUtilsTest {
 
     @ParameterizedTest(name = "Seconds {0} to time {1}")
-    @CsvSource({ "0, '00:00:00'", "3665, '01:01:05'", "7200, '02:00:00'", "32000, '-1'",
-                 "31999, '08:53:19'", "-1, '-1'" })
+    @CsvSource({ "0, '0:00:00'", "3665, '1:01:05'", "7200, '2:00:00'", "32000, '-1'",
+                 "31999, '8:53:19'", "-1, '-1'" })
     public void testSecToTime(int seconds, String expectedTime) {
         assertEquals(expectedTime, TimeUtils.secToTime(seconds));
     }
